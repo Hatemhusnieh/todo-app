01a9f0cf9867013c0deda5866ba9444c6f0f8fea
hatem <hatemhtm7@gmail.com>
2021-08-17 22:50:52 +0300

    lab 31 done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,29 @@
 import React, { useContext } from 'react';
 import { ListContext } from '../context/list';
+import { FormGroup, InputGroup, Switch, Intent, H1, Slider } from '@blueprintjs/core';
+import './form.scss';
 
 function Form() {
   const { handleSubmit, handleChange } = useContext(ListContext);
 
   return (
-    <div className="formContainer">
+    <div className="form1">
+      <H1>Add To Do Item</H1>
       <form onSubmit={handleSubmit}>
-        <h2>Add To Do Item</h2>
-
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
-
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
-
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
-        </label>
-
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
+        <FormGroup>
+          <FormGroup label="To-Do Details" labelInfo="(required)">
+            <InputGroup onChange={handleChange} name="text" placeholder="what do you want to do ?" intent={Intent.PRIMARY} />
+          </FormGroup>
+          <FormGroup label="Assignee Name" labelFor="text-input" labelInfo="(required)">
+            <InputGroup onChange={handleChange} name="assignee" placeholder="flan w 3lan" intent={Intent.PRIMARY} />
+          </FormGroup>
+          <FormGroup label="Difficulty" labelFor="range">
+            <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
+          </FormGroup>
+          <FormGroup>
+            <InputGroup type="submit" intent={Intent.DANGER} />
+          </FormGroup>
+        </FormGroup>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import './header.scss';
 function Header(props) {
   return (
-    <header>
-      <h1>To Do List: {props.incomplete} items pending</h1>
-    </header>
+    <>
+      <Navbar className="bp3-navbar bp3-dark">
+        <Navbar.Group>
+          <Navbar.Heading>To Do List: {props.incomplete} items pending</Navbar.Heading>
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
+      </Navbar>
+    </>
   );
 }
 
